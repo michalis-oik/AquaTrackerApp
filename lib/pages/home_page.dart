aa255851f5eb43f1e7ee5c673c0080cf2a22cef9
michalis-oik <mi8chal@gmail.com>
2025-09-14 15:53:41 +0300

    update alignment

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,6 +44,7 @@ class _HomePageState extends State<HomePage> {
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
